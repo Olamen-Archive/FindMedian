47cb18698b2ed4146d07f5aca6ed9b8e90a867a1
Zixuan Guo <i@zxguo.me>
2020-02-11 09:53:17 -0600

    add validity test;

--- src/main.c
+++ src/main.c
@@ -4,12 +4,30 @@
 #include <time.h>
 
 #include "function.h"
+#include "test.h"
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     shuffle(arr, 11);
     printArray(arr, 11);
 
-    printf("%d\n", findMedian(arr, 11));
+    printf("%d\n", isMedian(arr, 11, findMedian(arr, 11)));
     printArray(arr, 11);
+
+    for (int size = 1; size < 100000000; size += ) {
+        if (size % 2 == 0) { size += 1; }
+
+        int *arr;
+        generateArray(&arr, size);
+        int proposedMedian = findMedian(arr, size);
+
+        if (!isMedian(arr, size, proposedMedian)) {
+            printf("wrong!\n");
+            break;
+        }
+
+        free(arr); // GC
+    }
+
+    return 0;
 }
