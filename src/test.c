a43462ab7f3171897c7bb4304241db698919f7ac
Zixuan Guo <i@zxguo.me>
2020-02-11 09:52:32 -0600

    add array generator

--- src/test.c
+++ src/test.c
@@ -2,8 +2,8 @@
 // Created by Zixuan on 2/10/20.
 //
 
-#include "stdbool.h"
-#include "assert.h"
+#include <assert.h>
+#include <stdlib.h>
 
 #include "function.h"
 #include "test.h"
@@ -14,23 +14,34 @@
  * Notice the function also assume every element in arr is unique
  * and arr has odd number of elements.
  */
-bool isMedian(int arr[], int length, int median) {
+int isMedian(int arr[], int length, int median) {
     assert(length % 2 != 0);
 
     int scount = 0; // counter for element that smaller than median
     int bcount = 0;
 
     for (int i = 0; i < length; i++) {
-        assert(arr[i] != median);
+        //assert(arr[i] != median);
         if (arr[i] > median) {
             bcount += 1;
-        } else {
+        } else if (arr[i] < median) {
             // when arr[i] < median
             scount += 1;
         }
     }
 
     return scount == bcount;
+}
+
+void generateArray(int** arr, int size) {
+    assert(size %  2 != 0); // can only generate array with odd elements
+    *arr = malloc(sizeof(int) * size);
+
+    for (int i = 0; i < size; i++) {
+        (*arr)[i] = i; // ensure our generated array is half negative and half positive
+    }
+
+    shuffle(*arr, size);
 }
 
 
